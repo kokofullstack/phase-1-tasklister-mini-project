8e2c55514420c22f20b1bbe33f0f8ac7a7d8cfff
Maung Ko Ko <mwkoko2020@gmail.com>
2023-04-26 17:04:01 -0600

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    buildTaskList(e.target.querySelector('#new-task-description').value);
+    form.releasePointerCapture();
+  })
 });
